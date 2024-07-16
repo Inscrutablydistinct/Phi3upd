a9ed4ae3f337ecf298313d8e49215267d552d54b
Inscrutablydistinct <121133033+Inscrutablydistinct@users.noreply.github.com>
2024-07-16 12:05:06 +0530

    Update main.py

--- main.py
+++ main.py
@@ -124,7 +124,7 @@ The answer should only be a list and no other content whatsoever. Please print t
 
 list_of_documents = text_split.text_split(d)
 client = openai.OpenAI(
-    base_url="http://localhost:11434/v1",
+    base_url="https://localhost:11434/v1",
     api_key="nokeyneeded",
 )
 def ans(context, question):
