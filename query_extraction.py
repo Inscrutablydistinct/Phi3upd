f9714a4bd2c350bbd41f219b4702448e502ce7a0
Inscrutablydistinct <121133033+Inscrutablydistinct@users.noreply.github.com>
2024-07-16 12:57:56 +0530

    Update query_extraction.py

--- query_extraction.py
+++ query_extraction.py
@@ -12,7 +12,7 @@ def generate_md(Question, query):
     prompt = f"{Question}{query}"
     data = {
         "model": "phi3",
-        "temperature": 0.0001,
+        "temperature": 0.4,
         "n": 1,
         "messages": [
             {"role": "user", "content": prompt}
