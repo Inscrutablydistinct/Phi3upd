29c4e87d0b35528140c2657ed4abb0dfc2f1f1f5
Inscrutablydistinct <121133033+Inscrutablydistinct@users.noreply.github.com>
2024-07-19 12:05:22 +0530

    Update filter_by_metadata.py

--- filter_by_metadata.py
+++ filter_by_metadata.py
@@ -27,9 +27,9 @@ def filter_attributes(metadata_entry, key, value):
         cos_sim = compute_cosine_similarity(metadata_entry['keywords'], value)
         return cos_sim
     elif (key == 'publication_date'):
-        print(value)
         op = value[0] if value[1].isdigit() else value[0:2]
         value = value[len(op):]
+        print(value)
         filter_date = datetime.strptime(value, "%Y-%m-%d")
         if metadata_entry['publication_date'] == "N/A":
             return 0.0
